f1a91e74062f3c968d14a348c4e922fb1f9f360e
Tiago Mendes Bottamedi <tmbottamedi@mppr.mp.br>
2025-10-15 13:18:12 -0300

    Produção de mensagem com Kafka

--- produto-api/src/main/java/br/com/alura/comex/categoria/CategoriaController.java
+++ produto-api/src/main/java/br/com/alura/comex/categoria/CategoriaController.java
@@ -1,7 +1,12 @@
 package br.com.alura.comex.categoria;
 
 import java.util.List;
+import java.util.Properties;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -35,6 +40,24 @@ public class CategoriaController {
         Categoria categoria = request.toCategoria();
         categoriaService.cadastrar(categoria);
 
+        KafkaProducer kafkaProducer = new KafkaProducer<String, String>(properties());
+
+        ProducerRecord producerRecord = new ProducerRecord<>("PRODUTO_CATEGORIA_CRIADA", categoria.getNome());
+
+        try {
+            kafkaProducer.send(producerRecord, (data, ex) -> {
+                if (ex != null) {
+                    System.out.println("Erro ao enviar mensagem para o Kafka.");
+                    ex.printStackTrace();
+                    return;
+                }
+
+                System.out.println("Mensagem enviada para topic=" + data.topic() + ", key=" + categoria.getNome());
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return new ResponseEntity<>(categoria, HttpStatus.OK);
     }
 
@@ -42,5 +65,13 @@ public class CategoriaController {
     public ResponseEntity<List<Categoria>> lista() {
         List<Categoria> categorias = categoriaService.listar();
         return new ResponseEntity<>(categorias, HttpStatus.OK);
+    }
+
+    private Properties properties() {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
     }
 }
